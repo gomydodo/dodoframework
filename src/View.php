3606b8a9f048d9759a81129818cc925ad8cb2d4d
pengzw <1273230678@qq.com>
2014-10-22 14:50:15 +0800

    com

--- src/View.php
+++ src/View.php
@@ -15,7 +15,7 @@ class View{
 
 	private $config = array();
 
-	public function __construct($path='.'){
+	public function __construct($path = '.'){
 		self::$app = $this;
 		$this->path = $path;
 		$this->defaultConfig();
@@ -36,23 +36,35 @@ class View{
 		return isset($this->config[$key]) ? $this->config[$key] : null;
 	}
 
-	public function render($args=array()){
-		echo 'view/render';
+	public function render($file, $data=null){
+		$this->template = $this->getTemplate($file);
+
+        if (!file_exists($this->template)){
+            throw new \Exception("Template file not found: {$this->template}.");
+        }
+
+        if (is_array($data)) {
+            $this->vars = array_merge($this->vars, $data);
+        }
+
+        extract($this->vars);
+
+        include $this->template;
 	}
 
-	public function get(){
+	// public function exist($filename='.'){
+	// 	if(!file_exists($filename))
+			// $this->path = dirname(__DIR__);
+	// }
 
-	}
+	public function getTemplate($file) {
+		if((in_array(substr($file, 0,1), array('\\','/'))))
+			$file = substr($file, 1);
+		
+        if ((substr($file, -4) != '.php'))
+            $file .= '.php';
 
-	public function exist(){
-
-	}
-
-
-
-	private function ht($str){
-		return htmlspecialchars($str);
-	}
-
+        return $this->path.'/'.$this->getConfig('view.path').'/'.$file;
+    }
 
 }
